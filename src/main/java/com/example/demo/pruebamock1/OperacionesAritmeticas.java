174cd7674aa27158171f823cb234919952eeba49
Francisco Villa <francisco.villa.gonzalez@gmail.com>
2021-10-22 21:12:28 +0200

    delete

--- src/main/java/com/example/demo/pruebamock1/OperacionesAritmeticas.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.pruebamock1;
-
-public class OperacionesAritmeticas {
-
-    public double calculoFicticio(double cantidad){
-        System.out.println("OperacionesAritmeticas.calculoFicticio()");
-        return 1;
-    }
-}
